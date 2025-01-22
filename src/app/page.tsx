444ae5f4b30ca1bdbd71e28af18b71dbbbe532fb
oreo <Tang.Li5@geely.com>
2025-01-22 08:29:17 +0800

    feat: select mode remote or local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,58 @@
 'use client';
-import { Prefetch } from '@/components/prefetch';
-import { useVad } from '@/vad/use-vad';
-import { useWhisper } from '@/whisper/use-whisper';
 import { useState } from 'react';
+import Local from '@/components/localWhisper';
+import Remote from '@/components/remoteWhisper';
 
-export default function Local() {
-  const [transcription, setTranscription] = useState<String[]>([]);
-  const { remoteRun } = useWhisper();
-  const { recording, processing } = useVad({
-    onSpeechEnd: async ({ blob }) => {
-      const value = await remoteRun(blob);
-      if (value.trim()) {
-        setTranscription(pre => {
-          const temp = [...pre];
-          temp.push(value);
-          return temp;
-        });
-      }
-    },
-  });
+export default function Home() {
+  const [mode, setMode] = useState<'remote' | 'local' | null>(null);
+
+  const setModeToRemote = () => setMode('remote');
+
+  const setModeToLocal = () => setMode('local');
 
   return (
     <div className="w-[100vw] h-[100vh] p-4 bg-black">
-      <div className="rounded-2xl h-full ring-1 ring-zinc-800 px-8 py-8">
-        <div className="w-full flex items-center justify-between">
-          <div className={`flex items-center text-2xl font-semibold leading-none tracking-tight text-white`}>
-            Miles' Recorder
-          </div>
-          <div className="flex gap-2 items-center">
-            <div className="flex items-center gap-2 text-white px-3 py-1 rounded-full text-sm font-medium bg-gray-700 hover:bg-gray-600">
-              {recording ? (
-                <svg
-                  className="fill-red-500 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <circle cx="12" cy="12" r="10" />
-                </svg>
-              ) : (
-                <svg
-                  className={`size-4 fill-green-500 stroke-green-600 ${
-                    processing ? 'fill-amber-500 stroke-amber-600' : ''
-                  }`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M2 10v3" />
-                  <path d="M6 6v11" />
-                  <path d="M10 3v18" />
-                  <path d="M14 8v7" />
-                  <path d="M18 5v13" />
-                  <path d="M22 10v3" />
-                </svg>
-              )}
-              {recording ? 'Recording' : `Not Recording`}
+      {mode ? (
+        <>{{ remote: <Remote />, local: <Local /> }[mode]}</>
+      ) : (
+        <div className="rounded-2xl h-full ring-1 ring-zinc-800 px-8 py-8 flex flex-col">
+          <div className="w-full flex items-center justify-between pb-4">
+            <div className={`flex items-center text-2xl font-semibold leading-none tracking-tight text-white`}>
+              Miles' Recorder
             </div>
-            <Prefetch
-              models={[
-                {
-                  name: 'Download VAD',
-                  path: process.env.VAD_MODEL_PATH!,
-                },
-                // {
-                //   name: 'Download WHISPER',
-                //   path: process.env.WHISPER_MODEL_PATH!,
-                // },
-              ]}
-            />
           </div>
         </div>
+      )}
 
-        <ul className="space-y-2 text-white h-full overflow-y-scroll [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-neutral-700 [&::-webkit-scrollbar-thumb]:bg-neutral-500">
-          {transcription.length > 0 && transcription.map(i => <li className="p-3 bg-gray-800 rounded-md">{i}</li>)}
-        </ul>
+      <div
+        id="popup-modal"
+        tabIndex={-1}
+        className={`${
+          mode ? 'hidden' : ''
+        } bg-[#00000073] backdrop-blur overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
+        <div className="relative p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-[#0e1013] rounded-lg dark:bg-gray-700 shadow-2xl">
+            <div className="p-4 md:p-5 text-center">
+              <h3 className="mb-5 text-lg font-normal text-gray-300 dark:text-gray-400">
+                Welcome to Miles' Recorder !<div>Please choose a model</div>
+              </h3>
+              <button
+                data-modal-hide="popup-modal"
+                type="button"
+                className="ease-in-out duration-300 w-24	py-2.5 text-sm font-medium focus:outline-none border border-[#1C1E22] bg-[#1C1E22] text-[#ccc5d9] rounded-lg hover:bg-[#1C1E22] hover:text-[#ac5dd9] hover:border-[#ac5dd9] focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                onClick={setModeToRemote}>
+                remote
+              </button>
+              <button
+                data-modal-hide="popup-modal"
+                type="button"
+                className="ease-in-out duration-300 w-24 ms-3 text-white bg-[#AC5DD9] hover:bg-[#8644b3] focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm py-2.5"
+                onClick={setModeToLocal}>
+                local
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/localWhisper.tsx
@@ -0,0 +1,110 @@
+import { Prefetch } from '@/components/prefetch';
+import { useVad } from '@/vad/use-vad';
+import { useWhisper } from '@/whisper/use-whisper';
+import { useState } from 'react';
+
+export default function Local() {
+  const [transcription, setTranscription] = useState<String[]>([]);
+  const { localRun } = useWhisper();
+  const { recording, processing } = useVad({
+    onSpeechEnd: async ({ float32Array }) => {
+      let value = await localRun(float32Array);
+      if (value.trim()) {
+        // if (transcription.length > 0) {
+        //   const lastItem = transcription[transcription.length - 1];
+        //   const splitArr = value.split(lastItem as string);
+        //   console.log(splitArr);
+        // } else {
+        //   setTranscription(pre => {
+        //     const temp = [...pre];
+        //     temp.push(value);
+        //     return temp;
+        //   });
+        // }
+        setTranscription(pre => {
+          const temp = [...pre];
+          temp.push(value);
+          return temp;
+        });
+      }
+    },
+  });
+
+  return (
+    <div className="rounded-2xl h-full ring-1 ring-zinc-800 px-8 py-8 flex flex-col">
+      <div className="w-full flex items-center justify-between pb-4">
+        <div className={`flex items-center text-2xl font-semibold leading-none tracking-tight text-white`}>
+          Miles' Recorder
+        </div>
+        <div className="flex gap-2 items-center">
+          <Prefetch
+            models={[
+              {
+                name: 'Download VAD',
+                path: process.env.VAD_MODEL_PATH!,
+              },
+              {
+                name: 'Download WHISPER',
+                path: process.env.WHISPER_MODEL_PATH!,
+              },
+            ]}
+          />
+          <div className="flex items-center gap-2 text-white px-3 py-1 rounded-full text-sm font-medium bg-gray-700 hover:bg-gray-600">
+            {recording ? (
+              <svg
+                className="fill-red-500 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round">
+                <circle cx="12" cy="12" r="10" />
+              </svg>
+            ) : (
+              <svg
+                className={`size-4 fill-green-500 stroke-green-600 ${
+                  processing ? 'fill-amber-500 stroke-amber-600' : ''
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round">
+                <path d="M2 10v3" />
+                <path d="M6 6v11" />
+                <path d="M10 3v18" />
+                <path d="M14 8v7" />
+                <path d="M18 5v13" />
+                <path d="M22 10v3" />
+              </svg>
+            )}
+            {recording ? 'Recording' : `Not Recording`}
+          </div>
+        </div>
+      </div>
+
+      <ul
+        className={`flex flex-col items-center ${
+          transcription && transcription.length === 0 && 'justify-center'
+        } flex-1 space-y-2 text-white overflow-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-neutral-700 [&::-webkit-scrollbar-thumb]:bg-neutral-500`}>
+        {transcription.length > 0 ? (
+          transcription.map((text, index) => (
+            <li key={index} className="w-full p-3 bg-gray-800 rounded-md">
+              {text}
+            </li>
+          ))
+        ) : (
+          <div className="text-gray-400 text-lg">Speak please</div>
+        )}
+      </ul>
+    </div>
+  );
+}
